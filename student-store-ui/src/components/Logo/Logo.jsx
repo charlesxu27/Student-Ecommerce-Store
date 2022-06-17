df0912f191aec1d55bc908fdf2d61cf3f3b4583c
charlesxu27 <charlesxu27@gmail.com>
2022-06-17 18:53:20 +0200

    Add navbar + styling

--- student-store-ui/src/components/Logo/Logo.jsx
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -3,6 +3,6 @@ import "./Logo.css"
 
 export default function Logo() {
   return (
-    <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/wfsonfv0p92plryzwijf"/>
+    <img src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg"/>
   )
 }
