8d864e264ab610d1ae986b0f75caebabda0dccae
charlesxu27 <charlesxu27@gmail.com>
2022-06-17 19:24:14 +0200

    Add closed sidebar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -3,8 +3,24 @@ import "./Sidebar.css"
 
 export default function Sidebar() {
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className="sidebar closed">
+      <div className="wrapper">
+        <button className="toggle-button button closed">Forward Button</button>
+      <div className="shopping-cart">
+        <div className="cart-icons">
+          <span className="cart-icon icon button">
+            <i>Shopping Button</i>
+          </span>
+          <span className="cart-icon icon button">
+            <i>Money Button</i>
+          </span>
+          <span className="cart-icon icon button">
+            <i>Checkout Button</i>
+          </span>
+        </div>
+      </div>
+      </div>
     </section>
   )
 }
+
